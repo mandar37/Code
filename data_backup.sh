82d8d9f3e27f4ae8411c0edc2ce7e61c249b654e
Mandar Joshi <mandar37@gmail.com>
2014-05-26 12:55:48 +0530

    Update data_backup.sh

--- data_backup.sh
+++ data_backup.sh
@@ -1,3 +1,12 @@
+# For more information see the manual pages of crontab(5) and cron(8)
+# 
+# m h  dom mon dow   command
+52 16 * * * cd /home/username/ && sh data_backup.sh>>test.log| mail -s "Data Backup" youremail@gmail.com
+#
+
+
+
+
 #!/bin/sh
 notify-send "Data Backup"
 rsync -avzt /home/ /media/<path to backup>/
