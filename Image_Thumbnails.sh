05263f9e46e19ef207b1d4cbffe284d48999026b
Mandar Joshi <mandar37@gmail.com>
2014-07-23 11:51:51 +0530

    Update Image_Thumbnails.sh

--- Image_Thumbnails.sh
+++ Image_Thumbnails.sh
@@ -5,3 +5,16 @@ do
 echo "Prcoessing image $i ..."
 /usr/bin/convert -thumbnail 200 $i thumb.$i
 done
+
+
+#Install imagemagick, use apt-get command as follows:
+# apt-get install imagemagick
+
+#You need to pass -thumbnail argument to convert command:
+#convert thumbnail <width>x<height> image.png thumbnail.png
+
+#So to create a thumbnail of the abc.png image with 200px width, you need to type:
+#$ convert -thumbnail 200 abc.png thumb.abc.png
+
+#To create a thumbnail of the abc.png image with 200px height, you need to type:
+#$ convert -thumbnail x200 abc.png thumb.abc.png
