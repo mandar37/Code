02b4754606441588686e7a4190674aa2809aae29
Mandar Joshi <mandar37@gmail.com>
2014-08-25 14:00:57 +0530

    Update testping.sh

--- testping.sh
+++ testping.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-HOSTS="158.144.51.25
-158.144.51.34
-158.144.51.32
-158.144.51.65
-158.144.5.111
-158.144.5.112
-158.144.5.120
-158.144.5.121"
+HOSTS=" IP Address"
 DLIST=""
 for h in $HOSTS
 do
   fping -u $h >& /dev/null
   if [ $? -ne 0 ]; then
           echo ${h} host is down send email
-          #mail -s "Host ${h} down" kapil@theory.tifr.res.in </dev/null
+          mail -s "Host ${h} down" Email-id </dev/null
   fi
 done
